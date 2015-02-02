6011e8e1c06b284770db268a43bba0a32018d099
Zishan Ahmad <zahmad@teladoc.com>
2015-02-02 13:24:01 -0500

    add taskboard retrieval

---
+++ lib/leankitkanban/task_board.rb
@@ -0,0 +1,13 @@
+module LeanKitKanban
+  module TaskBoard
+    include HTTParty
+    include LeanKitRequest
+
+    FIND_TASK_BOARD = '/Board/{boardID}/Card/{cardID}/TaskBoard'
+
+    def self.find(board_id, card_id)
+      api_call = FIND_TASK_BOARD.gsub('{boardID}', board_id.to_s).gsub('{cardID}', card_id.to_s)
+      get(api_call)
+    end
+  end
+end
